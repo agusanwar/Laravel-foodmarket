e1e68d8e5eed911e92028c9ecba59eb05574e14e
agusanwar <agusanwar@gmail.com>
2022-02-24 09:58:24 +0700

    isadmin

--- app/Http/Middleware/IsAdmin.php
+++ app/Http/Middleware/IsAdmin.php
@@ -4,7 +4,6 @@ namespace App\Http\Middleware;
 
 use Closure;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Auth;
 
 class IsAdmin
 {
@@ -12,14 +11,14 @@ class IsAdmin
      * Handle an incoming request.
      *
      * @param  \Illuminate\Http\Request  $request
-     * @param  \Closure  $next
-     * @return mixed
+     * @param  \Closure(\Illuminate\Http\Request): (\Illuminate\Http\Response|\Illuminate\Http\RedirectResponse)  $next
+     * @return \Illuminate\Http\Response|\Illuminate\Http\RedirectResponse
      */
     public function handle(Request $request, Closure $next)
     {
-          if(Auth::user() && Auth::user()->roles == 'ADMIN'){
-            return $next($request);
-        }
-        return redirect('/');
+       if(Auth::user() && Auth::user()->roles == 'ADMIN') {
+           return $next($request);
+       }
+       return redirect('/');
     }
 }
